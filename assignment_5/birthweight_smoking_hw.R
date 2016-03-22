224557954239d527def8b50a3ac52ae52cd1f252
caimichael <michaeldcai@gmail.com>
2016-03-22 17:29:22 -0400

    added pdf

--- assignment_5/birthweight_smoking_hw.R
+++ assignment_5/birthweight_smoking_hw.R
@@ -15,3 +15,13 @@ dat <- read.csv("birthweight_smoking.csv",header=TRUE)
 fm0 <- lm(birthweight ~ smoker,data=dat)
 summary(fm0)
 coeftest(fm0,df=Inf,vcov=vcovHC(fm0))
+
+#second regression
+fm1 <- lm(birthweight ~ smoker + alcohol + nprevist, data=dat)
+summary(fm1)
+coeftest(fm1,df=Inf,vcov=vcovHC(fm1))
+
+#third regression
+fm2 <- lm(birthweight ~ smoker + alcohol + nprevist + unmarried, data=dat)
+summary(fm2)
+coeftest(fm2, df=Inf, vcov=vcovHC(fm2))
